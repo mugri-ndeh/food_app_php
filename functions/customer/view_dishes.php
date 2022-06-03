e024de60d2b88746e4d4457804776495363e71b1
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-03 06:25:18 +0100

    get all dishes

--- functions/customer/view_dishes.php
+++ functions/customer/view_dishes.php
@@ -1,3 +1,16 @@
 <?php
 
 include_once("../customer_functions.php");
+
+$id = $_POST['id'];
+   
+
+    $result = get_food_list($id);
+    //pt the result and name in associative array to send back to front end
+
+    $data = array(
+        'state'=>$result,
+    );
+    
+
+    echo json_encode($data);
