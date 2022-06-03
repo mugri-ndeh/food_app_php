5be8e16a2321263b16f9c6e95b631e8ec0357358
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-03 06:25:08 +0100

    sign up

--- functions/customer/signup.php
+++ functions/customer/signup.php
@@ -1,12 +1,12 @@
 <?php
 include_once("../customer_functions.php");
 
- $firstname = $_POST['firstname']
- $lastname = $_POST['lastname']
- $username = $_POST['username']
- $email = $_POST['email']
- $phonenumber = $_POST['phonenumber']
- $password = $_POST['password']
+ $firstname = $_POST['firstname'];
+ $lastname = $_POST['lastname'];
+ $username = $_POST['username'];
+ $email = $_POST['email'];
+ $phonenumber = $_POST['phonenumber'];
+ $password = $_POST['password'];
 
  $result = signup($firstname, $lastname, $username, $email, $phonenumber, $password);
 
