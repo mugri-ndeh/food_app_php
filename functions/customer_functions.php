e2183b1dd36dfe6bbf7e3d3c03ee2b7c9bdcb96d
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-03 06:24:45 +0100

    fixes  on queries

--- functions/customer_functions.php
+++ functions/customer_functions.php
@@ -1,55 +1,86 @@
 <?php
-include_once('../settings/connection.php');
-function signup($firstname, $lastname, $username, $email, $phonenumber, $password){
-    $sql = "INSERT INTO user (firstname, lastname, username email, phonenumber, password) VALUES (?, ?, ?, ?, ?, ?) ";
+include_once('../../settings/connection.php');
 
-    $stmt = $db->prepare($sql);
-    $result = $stmt->execute([$firstname, $lastname, $username $email, $phonenumber, $password]);
+function signup($firstname, $lastname, $username, $email, $phonenumber, $password){
+
+    $conn = openConn();
+    $sql = "INSERT INTO user (firstname, lastname, username, email, phonenumber, password) VALUES (?, ?, ?, ?, ?, ?) ";
+
+
+    $stmt = $conn->prepare($sql);
+    $res = $stmt->execute([$firstname, $lastname, $username, $email, $phonenumber, $password]);
+    $row = $stmt->rowCount();
+
+    $last_id =  $conn->lastInsertId();
+    $sql1 = "SELECT * FROM user WHERE id = ?";
 
 
    //if query works
-    if ($result) {
-
-        $data = $stmt->fetch(PDO::FETCH_ASSOC);
-        return json_encode(
-            [
-                'user' => $data
-            ]
-            );
+    if ($res) {
+        
+        echo('Result exists');
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return 'success';
+        }
        else {
           return 'failed';
        }
-    }
+    
 }
 
 
 function login($email, $password){
-    $sql = "SELECT * FROM user WHERE username = ? AND password = ?";
+    $conn = openConn();
 
-    $stmt = $db->prepare($sql);
-    $result = $stmt->execute([$username, $password]);
+    $sql = "SELECT * FROM user WHERE email = ? AND password = ?";
 
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(array($email, $password));
+
+    $row = $stmt->rowCount();
 
    //if query works
-    if ($result) {
-
+    if ($row>0) {
         $data = $stmt->fetch(PDO::FETCH_ASSOC);
-        return json_encode(
-            [
-                'user' => $data
-            ]
-            );
+   
+        return $data;
+    }
        else {
           return 'failed';
        }
-    }
+    
+}
+
+function getUser($id){
+    $conn = openConn();
+
+    $sql = "SELECT * FROM user WHERE id = ?";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(array($id));
+    $row = $stmt->rowCount();
+
+   //if query works
+    if ($row>0) {
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $data;
+        }
+       else {
+          return 'failed';
+       }
 }
 
 function create_order($foodItems, $qty, $state, $priceTotal){
-  $sql = "INSERT INTO orders (food_items, qty, o_state, price_total) VALUES ('$foodItems','$qty','$state','$priceTotal')"
+    $conn = openConn();
+
+  $sql = "INSERT INTO orders (food_items, qty, o_state, price_total) VALUES (?, ?, ?, ?)";
+
+  $stmt = $conn->prepare($sql);
+  $result = $stmt->execute([$foodItems, $qty, $state, $priceTotal]);
 
   $result = $conn->query($sql);
   if($result){
+      echo $result;
     return 'success';
    }
   
@@ -59,30 +90,59 @@ function create_order($foodItems, $qty, $state, $priceTotal){
 }
 
 function get_orders($userId){
-    $sql = "SELECT * FROM orders WHERE u_id = ?";
+    $conn = openConn();
+
+    $sql = "SELECT * FROM orders WHERE id = ?";
 
     $stmt = $db->prepare($sql);
-    $result = $stmt->execute([$userId]);
+    $stmt->execute([$userId]);
+    $row = $stmt->rowCount();
 
-
-    if($result){
-        $data = $stmt->fetch(PDO::FETCH_ASSOC);
-        return json_encode($data);
-    }
-    else {
-        return "failed";
-    }
+    //if query works
+     if ($row>0) {
+         $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+         return $data;
+     }
+        else {
+           return 'failed';
+        }
 }
 
 
-function get_food_list($category){
-  $sql = "SELECT * FROM food WHERE category = ?";
+function get_food_list($cat_id){
+    $conn = openConn();
 
+  $sql = "SELECT * FROM food WHERE cat_id = ?";
+
+  $stmt = $conn->prepare($sql);
+  
+  $stmt->execute([$cat_id]);
+  $row = $stmt->rowCount();
+
+  if ($row>0) {
+//       $arr_data = array(); 
+//       while(row>0){
+// $arr_data = 
+//       }
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// echo $data;
+    return $data;
+}
+   else {
+      return 'failed';
+   }
+}
+
+function joinquery($cat_id){
+    $conn = openConn();
+    $sql = 'SELECT * FROM food, food_category INNER JOIN food ON food.cat_id = food_category.id';
+    
   $stmt = $db->prepare($sql);
-  $result = $stmt->execute([$category]);
+  $result = $stmt->execute([$cat_id]);
 
   if($result){
-    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
     json_encode($data);
   }
   else {
