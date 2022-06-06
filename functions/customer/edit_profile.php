a3a1ecaba0abba202e11ebd9bc66759ca451e6aa
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-06 08:18:26 +0100

    edit profile init

---
+++ functions/customer/edit_profile.php
@@ -0,0 +1,19 @@
+<?php
+include_once("../customer_functions.php");
+
+ $firstname = $_POST['firstname'];
+ $lastname = $_POST['lastname'];
+ $username = $_POST['username'];
+ $email = $_POST['email'];
+ $phonenumber = $_POST['phonenumber'];
+ $id = (int)($_POST['id']);
+
+
+ $result = editProfile($firstname, $lastname, $username, $email, $phonenumber, $id);
+
+ $data = array(
+    'state'=>$result,
+);
+
+
+echo json_encode($data);
