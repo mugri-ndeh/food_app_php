4a35b1d2f9cd215079de024ae8a6ff760460161d
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-06 08:18:38 +0100

    search

---
+++ functions/customer/get_all_foods.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once("../customer_functions.php");
+
+$query = ucfirst(strtolower($_POST['query']));
+   
+
+    $result = getAllFoods($query);
+    //pt the result and name in associative array to send back to front end
+
+    $data = array(
+        'state'=>$result,
+    );
+    
+
+    echo json_encode($data);
