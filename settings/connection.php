aa05cf63335fe2a80c1c6f5036b56db334dcd5a4
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-07 03:53:02 +0100

    hotfix

--- settings/connection.php
+++ settings/connection.php
@@ -31,3 +31,31 @@
    
   return $conn;
 }
+
+/*
+<?php
+   // display errors if any
+   error_reporting(E_ALL);
+   ini_set('display_errors', 1);
+   
+   // function to open connection
+  function openConn(){
+   $host = "localhost";
+   $user = "id19059713_maestro";
+   $password = "<oX(=YX@3(iJfD%1";
+   $db = "id19059713_test_db";
+   
+//    if(!$conn){
+//       die("Connect failed:".mysqli_error());
+//    }
+  
+//    echo "Connected";
+//    return $conn;
+//   }
+
+  try
+  {
+    $conn = new PDO("mysql:host=$host; dbname=$db;charset=UTF8;", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+*/
