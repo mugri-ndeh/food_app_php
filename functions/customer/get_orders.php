d0cf5cbcdc1070149b94e79f722d20ccca472c0e
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-10 17:19:58 +0100

    fix. still issues with

--- functions/customer/get_orders.php
+++ functions/customer/get_orders.php
@@ -2,13 +2,25 @@
 include_once("../customer_functions.php");
 
     $id = $_POST['id'];
-   
+
+     // );
+
+
 
     $result = get_orders($id);
     //pt the result and name in associative array to send back to front end
+    $orders = [];
 
+    foreach($result as $item){
+     $fooditem = getOrderItem($item['id']);
+
+        array_push($orders, array(
+            'items'=> $fooditem,
+            'order'=>$item
+          ));
+    }
     $data = array(
-        'state'=>$result,
+        'state'=>$orders,
     );
     
 
