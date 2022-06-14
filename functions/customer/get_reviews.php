c1b3eda8a624e992280be72b17a17cdd2e85c66b
mugri-ndeh <frunwindeh@yahoo.com>
2022-06-14 23:13:33 +0100

    init lets seee

---
+++ functions/customer/get_reviews.php
@@ -0,0 +1,18 @@
+<?php
+include_once("../customer_functions.php");
+
+    $id = $_POST['id'];
+
+     // );
+
+
+
+    $result = getAllReviews();
+    //pt the result and name in associative array to send back to front end
+    
+    $data = array(
+        'state'=>$result,
+    );
+    
+
+    echo json_encode($data);
